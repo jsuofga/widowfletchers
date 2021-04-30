eb3e8792768ad1afa7d88360192364b226756f66
jeff su <jeff@octavainc.com>
2021-04-30 15:57:45 -0400

    center title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,8 @@
     <nav>
         <v-app-bar app flat >
             <v-app-bar-nav-icon text  @click = "leftDrawer = !leftDrawer"></v-app-bar-nav-icon>
-             <v-toolbar-title>Widow Fletcher AV Control App</v-toolbar-title>
+            <v-spacer></v-spacer>
+             <v-toolbar-title class="justify-center">Widow Fletcher AV Control App</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <DialogAdminLogIn />
                 <!-- <DialogIpAddress /> -->
